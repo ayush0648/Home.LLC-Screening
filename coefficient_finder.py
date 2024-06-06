0d265e4225cf141dd80f3b5b2c89d25417b9993a
ayush0648 <151781010+ayush0648@users.noreply.github.com>
2024-06-06 19:47:26 +0530

    Update coefficient_finder.py
    Added comments

--- coefficient_finder.py
+++ coefficient_finder.py
@@ -1,13 +1,11 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-# Load the filtered data
 filtered_data = pd.read_csv('combined_data_last_20_years_filtered.csv', parse_dates=['DATE'], index_col='DATE')
 
-# Define the target variable
 y = filtered_data['Home_Price_Index']
 
-# Initialize the dictionary to store coefficients
+#Dictionary to store all coefficient values.
 coefficients = {}
 
 # Perform linear regression separately for each factor
@@ -17,6 +15,5 @@ for factor in ['CPI', 'Federal_Funds_Rate', 'Housing_Inventory', 'Unemployment_R
     model.fit(X, y)
     coefficients[factor] = model.coef_[0]
 
-# Display the coefficients
 for factor, coef in coefficients.items():
     print(f"Coefficient for {factor}: {coef}")
